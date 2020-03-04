cf5c2df0aa0ec94c5be899e4f5a833d90b4624b9
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2020-03-04 15:00:46 -0500

    initial commit

---
+++ WorldTraveler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace WorldTraveler
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var cultures = new[] {"en", "es", "fr"};
+
+            foreach (var culture in cultures)
+            {
+                Language.Culture = CultureInfo.GetCultureInfo(culture);
+                Console.Write($"{Language.Culture.EnglishName} -" +
+                              $" {Language.Culture.NativeName} says ");
+                Console.Write($"{Language.HelloWorld}\n");
+            }
+
+            Console.WriteLine(Language.Woot);
+        }
+    }
+}
